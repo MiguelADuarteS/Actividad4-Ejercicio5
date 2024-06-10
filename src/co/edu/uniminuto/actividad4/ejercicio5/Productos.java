3712272a555d63fab87f05a4455ce8cbb80dd88f
mduar <mduar@MIGUEL-DUARTE>
2024-06-09 20:50:40 -0500

    Actividad4

--- src/co/edu/uniminuto/actividad4/ejercicio5/Productos.java
+++ src/co/edu/uniminuto/actividad4/ejercicio5/Productos.java
@@ -9,7 +9,53 @@ package co.edu.uniminuto.actividad4.ejercicio5;
  * @author mduar
  */
 public class Productos {
-    int precio; 
-    byte cantidad; 
-    String descripción;
+    private int precio; 
+    private byte cantidad; 
+    private String descripción;
+
+    public Productos() {
+    }
+    
+    public Productos(int precio, byte cantidad, String descripción) {
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.descripción = descripción;
+    }
+
+    public String getDescripción() {
+        return descripción;
+    }
+
+    public void setDescripción(String descripción) {
+        this.descripción = descripción;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public byte getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(byte cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Productos{");
+        sb.append("precio=").append(precio);
+        sb.append(", cantidad=").append(cantidad);
+        sb.append(", descripci\u00f3n=").append(descripción);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
 }
