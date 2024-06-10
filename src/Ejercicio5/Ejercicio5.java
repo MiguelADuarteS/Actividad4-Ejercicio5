06580639e6076e15addda510a97d7b59db151f4e
mduar <mduar@MIGUEL-DUARTE>
2024-06-09 20:38:25 -0500

    Actividad3

--- src/Ejercicio5/Ejercicio5.java
+++ src/Ejercicio5/Ejercicio5.java
@@ -9,5 +9,12 @@ package Ejercicio5;
  * @author mduar
  */
 public class Ejercicio5 {
+    /*5. Calcular el valor a pagar de un cliente por su compra, se desea saber los productos que ha
+comprado: precio, cantidad, descripción. También el total a pagar y cálculo de impuestos,
+adicional se debe tener el nombre, apellidos y documento del cliente.
+    */
     
+    public static void main(String[] args) {
+        
+    }
 }
