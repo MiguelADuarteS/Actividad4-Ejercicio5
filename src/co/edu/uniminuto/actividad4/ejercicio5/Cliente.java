47273ac3be56a0332186d7c95053d71646fcbbc0
mduar <mduar@MIGUEL-DUARTE>
2024-06-09 20:41:46 -0500

    Actividad3

--- src/co/edu/uniminuto/actividad4/ejercicio5/Cliente.java
+++ src/co/edu/uniminuto/actividad4/ejercicio5/Cliente.java
@@ -9,5 +9,53 @@ package co.edu.uniminuto.actividad4.ejercicio5;
  * @author mduar
  */
 public class Cliente {
+    private String nombre;
+    private String apellido;
+    private int documento;
+
+    public Cliente() {
+        
+    }
+    public Cliente(String nombre, String apellido, int documento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.documento = documento;
+    }
+
+    public int getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(int documento) {
+        this.documento = documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cliente{");
+        sb.append("nombre=").append(nombre);
+        sb.append(", apellido=").append(apellido);
+        sb.append(", documento=").append(documento);
+        sb.append('}');
+        return sb.toString();
+    }
+    
     
 }
